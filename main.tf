55b504bc0abdca799faadb158f40f4bdd747a435
RajaCloud002 <rajacloud002@gmail.com>
2023-01-20 22:27:03 +0530

    Module changes-alias

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
+provider "aws" {
+  region = "eu-west-1"
+  alias = "eu"
+}
+
 variable "instance_type" {
   type = string
 }
@@ -20,10 +25,12 @@ resource "aws_instance" "Server" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  providers = {
+    aws = "aws.eu"
+   }
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
+  name            = "my-vpc"
+  cidr            = "10.0.0.0/16"
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -36,6 +43,7 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
 output "public_ip" {
   value = aws_instance.Server.public_ip
 }
