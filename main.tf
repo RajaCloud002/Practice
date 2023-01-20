247b4bebfe9fff5210d9a77f30e73bcd7c7c2830
RajaCloud002 <rajacloud002@gmail.com>
2023-01-20 22:08:58 +0530

    Module addition

--- main.tf
+++ main.tf
@@ -18,6 +18,24 @@ resource "aws_instance" "Server" {
   }
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 output "public_ip" {
   value = aws_instance.Server.public_ip
 }
