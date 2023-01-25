5f4bbf67f26c6a21ffcb73d0d64784dbc14c3ee8
RajaCloud002 <rajacloud002@gmail.com>
2023-01-25 21:21:00 +0530

    s3 bucket-1

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -2,11 +2,17 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket-02132454"
+  acl    = "log-delivery-write"
+}
+
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-0213245"
+  bucket = "my-tf-test-bucket-012d2d2a1d"
   acl    = "private"
 
-  versioning {
-    enabled = true
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
   }
 }
